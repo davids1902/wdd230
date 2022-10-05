713238e3365dac23904267f0ed20fa4d926c0c3f
davids1902 <davids.carrera1902@gmail.com>
2022-10-05 09:10:45 -0500

    Change4

--- scripts/lu.js
+++ scripts/lu.js
@@ -1,15 +1,12 @@
+// This code will show the current YEAR beside the copyright mark. 
 const year = document.querySelector("#year");
 const YEAR = new Date();
 year.innerHTML= YEAR.toLocaleDateString('en-us',{year:'numeric'});
 
-
-let text = document.lastModified;
-//document.getElementById("currentDate").innerHTML = text;
-// document.querySelector("#currentDate") = "Hola";
-
+// This code  will show the last date modified of the document, on the footer.(Details are custom) 
 const LMsel = document.querySelector("#currentDate");
 const LMvar = new Date();
-const details = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const details = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric' };
 LMsel.innerHTML= LMvar.toLocaleDateString('en-us',details);
 
 
